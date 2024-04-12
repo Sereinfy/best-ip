2446b7d2db6355d6855288f2054b90004da74efb
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2024-04-12 20:26:04 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,7 @@ import os
 
 # 目标URL列表
 urls = ['https://monitor.gacjie.cn/page/cloudfront/ipv4.html' ]
-
+urls2 = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html' ]
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 
@@ -13,6 +13,9 @@ ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 if os.path.exists('ip.txt'):
     os.remove('ip.txt')
 
+if os.path.exists('front.txt'):
+    os.remove('front.txt')
+    
 # 创建一个文件来存储IP地址
 with open('ip.txt', 'w') as file:
     for url in urls:
@@ -38,3 +41,29 @@ with open('ip.txt', 'w') as file:
                 file.write(ip + '\n')
 
 print('IP地址已保存到ip.txt文件中。')
+
+# 创建一个文件来存储IP地址
+with open('front.txt', 'w') as file:
+    for url in urls2:
+        # 发送HTTP请求获取网页内容
+        response = requests.get(url)
+        
+        # 使用BeautifulSoup解析HTML
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # 根据网站的不同结构找到包含IP地址的元素
+        if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
+            elements = soup.find_all('tr')
+        else:
+            elements = soup.find_all('li')
+        
+        # 遍历所有元素,查找IP地址
+        for element in elements:
+            element_text = element.get_text()
+            ip_matches = re.findall(ip_pattern, element_text)
+            
+            # 如果找到IP地址,则写入文件
+            for ip in ip_matches:
+                file.write(ip + '\n')
+
+print('IP地址已保存到front.txt文件中。')
