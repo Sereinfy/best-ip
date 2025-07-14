6b0ffd37e51934b372adb4ec3bb0992757c5489b
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2025-07-14 20:58:24 +0800

    Update collect_ip.py

--- collect_ip.py
+++ collect_ip.py
@@ -5,7 +5,8 @@ import os
 
 # 目标URL列表
 urls = [
-    'https://www.wetest.vip/page/cloudflare/address_v4.html'
+    'https://www.wetest.vip/page/cloudflare/address_v4.html',
+    'https://ip.164746.xyz'
 ]
 
 # 正则表达式用于匹配IP地址
@@ -42,7 +43,7 @@ with open('ip.txt', 'a') as file:  # 使用追加模式写入
                 
                 # 如果找到IP地址，则写入文件
                 for ip in ip_matches:
-                    file.write(ip + '\n')
+                    文件。write(ip + '\n')
         except requests.exceptions.RequestException as e:
             print(f"访问 {url} 时出错: {e}")
 
