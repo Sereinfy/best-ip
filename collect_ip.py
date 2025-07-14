d07c421df6448fd4eb62de00dd4f305556cf8ba7
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2025-07-14 21:05:35 +0800

    Update collect_ip.py

--- collect_ip.py
+++ collect_ip.py
@@ -4,9 +4,7 @@ import re
 import os
 
 # 目标URL列表
-urls = [
-    'https://api.uouin.com/cloudflare.html'
-]
+urls = ['https://ipdb.030101.xyz/bestcfv4/']
 
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
