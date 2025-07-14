5a538a5f3c9e990a965253b8ccf9ff542e638ea8
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2025-07-14 21:06:21 +0800

    Update collect_ip.py

--- collect_ip.py
+++ collect_ip.py
@@ -4,7 +4,7 @@ import re
 import os
 
 # 目标URL列表
-urls = ['https://ipdb.030101.xyz/bestcfv4/']
+urls = ['https://www.wetest.vip/page/cloudflare/address_v4.html']
 
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
